840af73f2eb89c1acca09a506d6e38a576ba92de
Santiago González <sagohe28@gmail.com>
2025-05-01 12:04:16 -0500

    Agrego STATICFILES_STORAGE para producción

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
