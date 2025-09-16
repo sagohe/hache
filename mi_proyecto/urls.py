50f9e3d966572fd738f4d4f2fffc6fcd966768c3
Santiago González <sagohe28@gmail.com>
2025-09-16 11:56:32 -0500

    Actualizacion en autenticacion

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -1,16 +1,16 @@
-
 from django.contrib import admin
 from django.urls import path, include
-from mi_app.views import exportar_horarios_pdf, panel_inicio
 from django.views.generic import RedirectView
 
 
 
 urlpatterns = [
-    path('', RedirectView.as_view(pattern_name='admin:index', permanent=False)),
+    # root -> register (landing para nuevos usuarios)
+    path('', RedirectView.as_view(pattern_name='register', permanent=False)),
 
-    path('', include('mi_app.urls')),    
+    # rutas de la app (login, register, etc.)
+    path('', include('mi_app.urls')),
+
+    # admin al final
     path('admin/', admin.site.urls),
 ]
-
-
